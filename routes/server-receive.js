7f2a1845a5525cef27e1eed124445e0baba555f2
Clayton Petty <git.notyalc@gmail.com>
2014-03-24 23:02:21 -0500

    more more

--- routes/server-receive.js
+++ routes/server-receive.js
@@ -12,13 +12,13 @@ module.exports = function(req, res){
   console.log('Attempted upload from username:', req.body.username, 'and password', req.body.password);
   
   //Check for required username and password
-  if (req.body.username && req.body.password){
+  if (req.params.username && req.body.password){
     
     //check for file to be uploaded
     if (req.files.file){
       
       //find the user requesting to upload
-      users.find({ username: req.body.username, password: req.body.password }, function(err, found){
+      users.find({ username: req.params.username, password: req.body.password }, function(err, found){
         if (err) res.send(err);
         else{
           
@@ -31,19 +31,22 @@ module.exports = function(req, res){
             new_file.date = new Date(); 
             new_file.user = found._id;
             
-            //Save the new submission data
-            new_file.save(function(err){
-              if (err) res.send(err);
-              else{
+
                 
-                //Save and store the file
-                var resolved_path = path.resolve('storage', req.body.username);
-                util.save(req.files.file, resolved_path, req.files.file.name, function(err){
-                  if (err) res.send(err);
-                  else res.send('File saved');
-                });
-              }
-            });
+              //Save and store the file
+              var resolved_path = path.resolve('storage', req.body.username);
+              util.save(req.files.file, resolved_path, req.files.file.name, function(err){
+                if (err) res.send(err);
+                else{
+                  //Save the new submission data
+                  new_file.save(function(err){
+                    if (err) res.send(err);
+                    else{
+                      res.send('File saved');
+                    }
+                  });
+                }
+              });
           }
         }
       });

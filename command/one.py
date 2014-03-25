a02ec2db4a4634dd801ba613742596b937e3793d
Clayton Petty <git.notyalc@gmail.com>
2014-03-24 23:01:03 -0500

    more

--- command/one.py
+++ command/one.py
@@ -11,56 +11,74 @@
   #(_______)|/    )_)(_______/ #
    ############################                       
 
-from optparse import OptionParser
+from optparse import OptionParser, OptionGroup
 import signal, sys
 
-def pull(parser):
-  
-  # Defaults
-  parser.set_defaults(dpath='.')
-  parser.set_defaults(stdout=False)
-  
-  # Options
-  parser.add_option('-d', '--download-path', 
-                    action='store', type='string', dest='dpath',
-                    help='Specify the folder to place file in')
-  parser.add_option('-o', '--stdout',
-                    action='store', type='store_true', dest='stdout',
-                    help='Write the files contents to stdout')
-  # complete
-  return parser
+SUB_COMMANDS = ['push', 'pull', 'list']
+NEED_INPUT_STRING = ['push', 'pull']
 
-def push(parser):
+def log(statement):
+  sys.stdout.write(statement)
 
-  # Defaults
-  parser.set_defaults()
-  
-  # Options
-  parser.add_option('-r', '--recurse',
-                    action='store', type-'store_true', dest='recurse',
-                    help='recuse into directory and push all files')
-  
+def die(statement):
+  sys.stderr.write('ERROR: ' + statement + '\n')
+  sys.exit(1)
 
-def composeOptions(sub_command):
+def pull():
+  pass
+
+def push():
+  pass
+ 
+
+def build_option_parser():
   
-  # Create option parser
+  # Create option parser and Option Groups
   usage = "usage: %prog <command> [options] arg1"
   version = "%prog 0.1"
   parser = OptionParser(usage=usage, version=version)
+  push_group = OptionGroup(parser, 'Pull Options', 
+                           'These options apply to only the "Pull" subcommand')
+  pull_group = OptionGroup(parser, 'Pull Options', 
+                           'These options apply to only the "Pull" subcommand')
   
-  # Global Defaults
-  parser.set_defaults(quiet=False)
+  # Defaults
+  parser.set_defaults(quiet=False) #Global
+  parser.set_defaults(recurse=False) #Push
+  parser.set_defaults(dpath='.', stdout=False) #Pull
   
-  # Global Options
+  
+  # Options
   parser.add_option('-q', '--quiet',
-                    action='store', type='store_true', dest='quiet',
-                    help='Limit ouput to stdout')
-  
+                    action='store_true', dest='quiet',
+                    help='Limit ouput to stdout') 
+  push_group.add_option('-r', '--recurse',
+                    action='store_true', dest='recurse',
+                    help='recuse into directory and push all files') 
   # COMPLETE VERSION OPTION
-  parser.add_option('-v', '--version')
+  pull_group.add_option('-d', '--download-path', 
+                    action='store', type='string', dest='dpath',
+                    help='Specify the folder to place file in')
+  pull_group.add_option('-o', '--stdout',
+                    action='store_true', dest='stdout',
+                    help='Write the files contents to stdout')
   
+  # Add option groups
+  parser.add_option_group(push_group)
+  parser.add_option_group(pull_group)
+
+  return parser
+
+def main():
   
-                      
+  (options, args) = build_option_parser().parse_args()
+  
+  if len(args) == 0:
+    die('No sub-command chosen')
+  elif len(args) == 1 and args[0] in NEED_INPUT_STRING:
+    die('No file selected. Must select at least one file')
+  elif args[0] not in SUB_COMMANDS:
+    die('Invalid sub-command chosen: ' + args[0])
 
   
 if __name__ =='__main__':

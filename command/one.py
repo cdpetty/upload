8f437a9b2f756473106fd42692a25ea9efcc98d2
Clayton Petty <git.notyalc@gmail.com>
2014-03-26 19:28:10 -0500

    stuff

--- command/one.py
+++ command/one.py
@@ -12,10 +12,12 @@
    ############################                       
 
 from optparse import OptionParser, OptionGroup
-import signal, sys, requests
+from os import path
+import signal, sys, requests, getpass
 
-SUB_COMMANDS = ['push', 'pull', 'list']
+SUB_COMMANDS = ['push', 'pull', 'list', 'auth']
 NEED_INPUT_STRING = ['push', 'pull']
+URL = 'http://localhost:3000'
 
 def log(statement):
   sys.stdout.write(statement)
@@ -23,13 +25,6 @@ def log(statement):
 def die(statement):
   sys.stderr.write('ERROR: ' + statement + '\n')
   sys.exit(1)
-
-def pull():
-  pass
-
-def push():
-  pass
- 
 
 def build_option_parser():
   
@@ -55,7 +50,6 @@ def build_option_parser():
   push_group.add_option('-r', '--recurse',
                     action='store_true', dest='recurse',
                     help='recuse into directory and push all files') 
-  # COMPLETE VERSION OPTION
   pull_group.add_option('-d', '--download-path', 
                     action='store', type='string', dest='dpath',
                     help='Specify the folder to place file in')
@@ -69,20 +63,46 @@ def build_option_parser():
 
   return parser
 
+def obtain_user_info():
+  # Check if authentication file exists
+  p = path.expanduser('~/.one')
+  if path.isfile(p):
+    username = ''
+    password = ''
+    with open(p, 'r') as auth_file:
+      auth_file = open(p, 'r')
+      username = auth_file.readline().strip()
+      password = auth_file.readline()
+    return (username, password)
+  else:
+    initialize()
+  die('done')
+    
+def initialize():
+  # create new user
+  username = raw_input('If you have an account already, input your username: ')
+  password = getpass.getpass('Input your password (note: no text will appear on screen): ')
+  
+  # Create authentication file
+  p = path.expanduser('~/.one')
+  with open(p, 'w') as auth_file:
+    auth_file.write(username + '\n' + password)
+  
+  log('Consider yourself: Signed In')
+  sys.exit(0)
+
 def upload(filenames): 
-  url = "http://localhost:3000"
   route = "/receive/clayton"
   data = {'username':'clayton', 'password':'petty'}
   for file in filenames:
     files = {'file':open(file, 'r')}
-    r = requests.post(url + route, files=files, data=data)
+    r = requests.post(URL + route, files=files, data=data)
     print r.text
 
 def download(filenames):
-  url = "http://localhost:3000"
   for filename in filenames:
     route = "/send/clayton/" + filename
-    file = requests.get(url + route).text
+    file = requests.get(URL + route).text
     f = open(filename, 'w')
     f.write(file)
     f.close()
@@ -99,11 +119,14 @@ def main():
     die('Invalid sub-command chosen: ' + args[0])
   
   sub_command = args[0]
+  files = args[1:]
   
   if sub_command == 'pull':
     download(args[1:])
   elif sub_command == 'push':
     upload(args[1:])
+  elif sub_command == 'init':
+    obtain_user_info()
   
 
   
